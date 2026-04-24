ea2e28d9502f621e36465d701d2bcc9440d3d2ef
Mohamed Aljarawneh <mohammed@gp.com>
2026-04-24 10:54:46 -0400

    Fix cache: fixed build ID forces browser to load new JS bundles

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
   images: { unoptimized: true },
+  generateBuildId: async () => "v3-mushaf-text",
 };
 
 export default nextConfig;
